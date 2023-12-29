d02f2c7ece53d1b563260c2823dd90f0d9fdbcc8
nathan-lesny <nlesny15@gmail.com>
2023-12-28 23:13:57 -0500

    Initial Commit

---
+++ LilBud/LilBudApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilBudApp.swift
+//  LilBud
+//
+//  Created by Nathan Lesny on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct LilBudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
